d9bd1095b608df348b8a972fed6438583dea8b83
Sergey Pushkovskiy <sergey@MacBook-Pro-User.local>
2020-03-25 22:10:15 +0200

    changed gitignore

---
+++ dist/precache-manifest.4a1328ab519a42aa71bbb4d3a6405aa9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63d51fdc5e04c692ee42",
+    "url": "/css/app.a7fa69f5.css"
+  },
+  {
+    "revision": "8aa4851df45f3ddafe79",
+    "url": "/css/chunk-vendors.c943da6a.css"
+  },
+  {
+    "revision": "ad57d120c60711fb727529a18278baee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63d51fdc5e04c692ee42",
+    "url": "/js/app.07d62067.js"
+  },
+  {
+    "revision": "8aa4851df45f3ddafe79",
+    "url": "/js/chunk-vendors.dcc07db0.js"
+  },
+  {
+    "revision": "577509102ffa4319e54fdb1f2685bab6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
